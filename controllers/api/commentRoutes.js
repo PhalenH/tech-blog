2bf82b7acb9079e93b6cb1a99c4ed690e5e9f6ad
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-12-09 22:45:42 -0500

    added comment and post routes to api index.js, filled in post and comment .js files inside api folder

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,58 @@
+const commentRoutes = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+commentRoutes.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    if (!commentData) {
+      res.status(400).json("No users found!");
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// create a comment
+commentRoutes.post("/:id", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      post_id: req.params.id,
+    });
+    // why do i need ...req.body? req.body doesn't work
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// delete a comment
+commentRoutes.delete("/:id/comment", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: "No comments found with this ID!" });
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// update a comment
+
+module.exports = commentRoutes;
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,75 @@
+const postRoutes = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+postRoutes.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    if (!postData) {
+      res.status(400).json("No users found!");
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// create a post
+postRoutes.post("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id
+    });
+    // why do i need ...req.body? req.body didn't work
+
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// update a post by id
+postRoutes.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No posts found with this ID!" });
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// delete a post by id
+postRoutes.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No posts found with this ID!" });
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = postRoutes;
