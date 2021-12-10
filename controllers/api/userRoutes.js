0b045c6e602357e5fbef26d3b6e00d28d0818217
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-12-09 21:01:25 -0500

    added get all for api/userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,8 +3,23 @@ const { User } = require("../../models");
 
 // url at this point is: /api/users
 
+// gets all user, test to see if it works
+userRoutes.get("/", async (req, res) => {
+  try {
+    const userData = await userRoutes.findAll();
+    if (!userData) {
+      res.status(400).json("No users found!");
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // logout user
-userRoutes.post('/logout', (req, res) => {
+userRoutes.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -17,16 +32,12 @@ userRoutes.post('/logout', (req, res) => {
 //create a new user
 userRoutes.post("/", async (req, res) => {
   try {
-    const userData = await User.create({
-      username: req.body_username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.loggedIn = true;
       req.session.user_id = userData.id;
-      
+
       res.status(200).json(userData);
     });
   } catch (err) {
