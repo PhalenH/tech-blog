59ab0b20b67c0c7d7d991f5223eac9e2c1aec61c
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-12-11 09:16:01 -0500

    modified seeds

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -3,31 +3,37 @@ const { Comment } = require("../models");
 const commentData = [
   {
     comment_content: "me too!",
+    date_created: 8/12/2021,
     user_id: 4,
     post_id: 1
   },
   {
     comment_content: "It's a scam",
+    date_created: 8/12/2021,
     user_id: 3,
     post_id: 2
   },
   {
     comment_content: "They are helpful",
+    date_created: 8/12/2021,
     user_id: 2,
     post_id: 3
   },
   {
     comment_content: "I like tim ferris's pod about this topic",
+    date_created: 8/12/2021,
     user_id: 1,
     post_id: 4
   },
   {
-    comment_content: "Shit is pricey, debating buying it everyday.",
-    user_id: 4,
+    comment_content: "It's is pricey, debating buying it everyday.",
+    date_created: 8/12/2021,
+    user_id: 3,
     post_id: 1
   },
   {
-    comment_content: "Fuck ben simmons",
+    comment_content: "Forget ben simmons, dude is a bum",
+    date_created: 8/12/2021,
     user_id: 2,
     post_id: 6
   },
